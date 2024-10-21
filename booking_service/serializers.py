a1e0867ee9a9092e36c1e3e57653eef2d8e29567
AndriiIshchenko <iscenkoa@gmail.com>
2024-10-21 10:56:04 +0300

    feat: Add base serializers to booking_service

---
+++ booking_service/serializers.py
@@ -0,0 +1,60 @@
+from rest_framework import serializers
+
+from booking_service.models import (
+    Crew,
+    Order,
+    Route,
+    Station,
+    Ticket,
+    Train,
+    TrainType,
+    Trip,
+)
+
+
+class CrewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Crew
+        fields = ("id", "first_name", "last_name")
+
+
+class StationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Station
+        fields = ("id", "name", "latitude", "longitude")
+
+
+class TrainTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TrainType
+        fields = ("id", "name")
+
+
+class TrainSerilizer(serializers.Serializer):
+    class Meta:
+        model = Train
+        fields = ("id", "name", "cargo_num", "places_in_cargo", "train_type")
+
+
+class RouteSerialixer(serializers.Serializer):
+    class Meta:
+        model = Route
+        fields = ("id", "source", "destination", "distance")
+
+
+class TripSerializer(serializers.Serializer):
+    class Meta:
+        model = Trip
+        fields = ("id", "route", "train", "departure_time", "arrival_time", "crew")
+
+
+class OrderSerializer(serializers.Serializer):
+    class Meta:
+        model = Order
+        fields = ("id", "created_at", "user")
+
+
+class TicketSerializer(serializers.Serializer):
+    class Meta:
+        model = Ticket
+        fields = ("id", "cargo", "seat", "trip", "order")
