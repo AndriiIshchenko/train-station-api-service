8591f15a804f318e3cf0464bcfe12cecd43ba1e1
AndriiIshchenko <iscenkoa@gmail.com>
2024-10-21 10:35:16 +0300

    feat: Register booking_service models in admin

--- booking_service/admin.py
+++ booking_service/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Route,
+    Crew,
+    Ticket,
+    Train,
+    TrainType,
+    Order,
+    Trip,
+    Station,
+)
+admin.site.register(Route)
+admin.site.register(Crew)
+admin.site.register(Ticket)
+admin.site.register(Train)
+admin.site.register(TrainType)
+admin.site.register(Order)
+admin.site.register(Trip)
+admin.site.register(Station)
