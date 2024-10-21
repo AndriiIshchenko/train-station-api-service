643346a9417a24d4f4eff551f7e58e595a8ad51f
AndriiIshchenko <iscenkoa@gmail.com>
2024-10-21 13:55:11 +0300

    feat: Add urls to booking_service

---
+++ booking_service/urls.py
@@ -0,0 +1,31 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from booking_service.views import (
+    CrewViewSet,
+    OrderViewSet,
+    RouteViewSet,
+    StationViewSet,
+    TicketViewSet,
+    TrainTypeViewSet,
+    TrainViewSet,
+    TripViewSet,
+)
+
+
+router = routers.DefaultRouter()
+
+router.register("crew", CrewViewSet)
+router.register("stations", StationViewSet)
+router.register("trains", TrainViewSet)
+router.register("train_types", TrainTypeViewSet)
+router.register("routes", RouteViewSet)
+router.register("trips", TripViewSet)
+router.register("orders", OrderViewSet)
+router.register("tickets", TicketViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
+app_name = "booking_service"
--- train_station_service/urls.py
+++ train_station_service/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("api/user", include("user.urls", namespace="user")),
-    path("api/booking_service", include("booking_service.urls", namespace="booking_service")),
+    path("admin/", admin.site.urls),
+    path("api/user/", include("user.urls", namespace="user")),
+    path(
+        "api/booking_service/",
+        include("booking_service.urls", namespace="booking-service"),
+    ),
 ]
