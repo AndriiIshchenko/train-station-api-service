a3aeaffb62521595716f351923e0a7c7b84c9634
AndriiIshchenko <iscenkoa@gmail.com>
2024-11-04 17:42:41 +0200

    feat: Add filtering for Trip

--- booking_service/views.py
+++ booking_service/views.py
@@ -1,6 +1,9 @@
+from datetime import datetime
 from django.db.models import F, Count
 from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 
 from booking_service.models import (
     Crew,
@@ -70,16 +73,35 @@ class TripViewSet(
     mixins.RetrieveModelMixin,
     GenericViewSet,
 ):
-    queryset = (Trip.objects.all()
-    .select_related("train", "route")
-    .annotate(
-        tickets_available=(
-            F("train__cargo_num") * F("train__places_in_cargo")
-            - Count("tickets")
-        )
+    queryset = (
+        Trip.objects.all()
+        .select_related("train", "route")
+        .annotate(
+            tickets_available=(
+                F("train__cargo_num") * F("train__places_in_cargo") - Count("tickets")
+            )
         )
     )
-    serializer_class = TripSerializer
+    # serializer_class = TripSerializer
+
+    def get_queryset(self):
+        date = self.request.query_params.get("departure_time")
+        source_id_str = self.request.query_params.get("source")
+        destination_id_str = self.request.query_params.get("destination")
+
+        queryset = self.queryset
+
+        if date:
+            date = datetime.strptime(date, "%Y-%m-%d").date()
+            queryset = queryset.filter(departure_time__date__gte=date)
+
+        if source_id_str:
+            queryset = queryset.filter(route__source__id=int(source_id_str))
+
+        if destination_id_str:
+            queryset = queryset.filter(route__destination__id=int(destination_id_str))
+
+        return queryset
 
     def get_serializer_class(self):
         if self.action == "list":
@@ -87,6 +109,28 @@ class TripViewSet(
         if self.action == "retrieve":
             return TripDetailSerializer
         return TripSerializer
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "source",
+                type=OpenApiTypes.INT,
+                description="Filter by trip source id (ex. ?source=2)",
+            ),
+            OpenApiParameter(
+                "destination",
+                type=OpenApiTypes.INT,
+                description="Filter by trip destination id (ex. ?destination=2)",
+            ),
+            OpenApiParameter(
+                "departure_time",
+                type=OpenApiTypes.DATE,
+                description=("Filter by datetime of Trip " "(ex. ?date=2022-10-23)"),
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
 
 class OrderViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, GenericViewSet):
@@ -96,12 +140,12 @@ class OrderViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, GenericViewSe
     serializer_class = OrderSerializer
 
     def get_queryset(self):
-        return Order.objects.filter(user=self.request.user)
+        return self.queryset.filter(user=self.request.user)
 
     def get_serializer_class(self):
         if self.action == "list":
             return OrderListSerializer
-        return super().get_serializer_class()
+        return OrderSerializer
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
